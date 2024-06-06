ffce4a1678e805500bcaf8b2123e59d8c4593004
Amine <aouraghamine406@gmail.com>
2024-06-06 16:27:56 +0200

    Sprint 4

--- src/Inkooporder/insert.php
+++ src/Inkooporder/insert.php
@@ -1,35 +1,4 @@
 <?php
-namespace Bas\classes;
-
-class Inkooporder {
-    private $pdo;
-
-    public function __construct() {
-        $this->pdo = new \PDO('mysql:host=your_host;dbname=your_db', 'your_user', 'your_password');
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    }
-
-    public function insertInkooporder($inkoopordergegevens) {
-        $sql = "INSERT INTO inkooporders (levId, artId, inkOrdDatum, inkOrdBestAantal, inkOrdStatus) VALUES (:levId, :artId, :inkOrdDatum, :inkOrdBestAantal, :inkOrdStatus)";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute($inkoopordergegevens);
-        return $stmt->rowCount();
-    }
-
-    public function getLastInsertedId() {
-        return $this->pdo->lastInsertId();
-    }
-}
-?>
-Ensure Correct Namespacing and Autoloading:
-Make sure the namespaces and autoloading are correctly set up. This usually involves having a proper composer.json file and running composer dump-autoload.
-
-Update insert.php:
-With the class definitions corrected, here's the final version of insert.php:
-
-php
-Code kopiëren
-<?php
 // Auteur: Amin
 // Functie: Insert
 
--- src/Verkooporder/insert.php
+++ src/Verkooporder/insert.php
@@ -9,63 +9,42 @@ use Bas\classes\Verkooporder;
 
 $message = "";
 
-if (isset($_POST["insert"]) && $_POST["insert"] == "Toevoegen") {
-    if (
-        isset($_POST['klantnaam']) && isset($_POST['klantemail']) && isset($_POST['klantadres']) && isset($_POST['klantpostcode']) &&
-        isset($_POST['artikelnaam']) && isset($_POST['artikelprijs']) && isset($_POST['verkOrdDatum']) &&
-        isset($_POST['verkOrdBestAantal']) && isset($_POST['verkOrdStatus'])
-    ) {
+// Maak nieuwe objecten aan
+$klant = new Klant();
+$artikel = new Artikel();
+$verkooporder = new Verkooporder();
 
-        // Maak een nieuw Klant object aan
-        $klant = new Klant();
-        // Maak een nieuw Artikel object aan
-        $artikel = new Artikel();
-        // Maak een nieuw Verkooporder object aan
-        $verkooporder = new Verkooporder();
+$klanten = $klant->getKlanten(); // Hier wordt de getKlanten methode opgeroepen
 
-        // Bereid de klantgegevens voor
-        $klantgegevens = [
-            'klantNaam' => $_POST['klantnaam'],
-            'klantEmail' => $_POST['klantemail'],
-            'klantWoonplaats' => $_POST['klantwoonplaats'],
-            'klantAdres' => $_POST['klantadres'],
-            'klantPostcode' => $_POST['klantpostcode']
+$artikelen = $artikel->getArtikelen();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["insert"]) && $_POST["insert"] == "Toevoegen") {
+    $requiredFields = [
+        'klantId', 'artId', 'verkOrdDatum', 'verkOrdBestAantal', 'verkOrdStatus'
+    ];
+    $isFormValid = true;
+
+    foreach ($requiredFields as $field) {
+        if (empty($_POST[$field])) {
+            $isFormValid = false;
+            break;
+        }
+    }
+
+    if ($isFormValid) {
+        // Bereid de verkoopordergegevens voor
+        $verkoopordergegevens = [
+            'klantId' => intval($_POST['klantId']),
+            'artId' => intval($_POST['artId']),
+            'verkOrdDatum' => $_POST['verkOrdDatum'],
+            'verkOrdBestAantal' => intval($_POST['verkOrdBestAantal']),
+            'verkOrdStatus' => intval($_POST['verkOrdStatus'])
         ];
 
-        // Bereid de artikelgegevens voor
-        $artikelgegevens = [
-            'artikelNaam' => $_POST['artikelnaam'],
-            'artikelPrijs' => $_POST['artikelprijs']
-        ];
-
-        // Voeg de klantgegevens toe aan de database
-        if ($klant->insertKlant($klantgegevens)) {
-            // Haal het laatste klant ID op
-            $klantId = $klant->getLastInsertedId();
-
-            // Voeg de artikelgegevens toe aan de database
-            if ($artikel->insertArtikel($artikelgegevens)) {
-                // Haal het laatste artikel ID op
-                $artikelId = $artikel->getLastInsertedId();
-
-                // Voeg de verkooporder toe aan de database
-                $verkoopordergegevens = [
-                    'klantId' => $klantId,
-                    'artikelId' => $artikelId,
-                    'verkOrdDatum' => $_POST['verkOrdDatum'],
-                    'verkOrdBestAantal' => $_POST['verkOrdBestAantal'],
-                    'verkOrdStatus' => $_POST['verkOrdStatus']
-                ];
-                if ($verkooporder->insertVerkooporder($verkoopordergegevens)) {
-                    $message = "Verkooporder succesvol toegevoegd!";
-                } else {
-                    $message = "Er is een fout opgetreden bij het toevoegen van de verkooporder.";
-                }
-            } else {
-                $message = "Er is een fout opgetreden bij het toevoegen van het artikel.";
-            }
+        if ($verkooporder->insertVerkooporder($verkoopordergegevens)) {
+            $message = "Verkooporder succesvol toegevoegd!";
         } else {
-            $message = "Er is een fout opgetreden bij het toevoegen van de klant.";
+            $message = "Er is een fout opgetreden bij het toevoegen van de verkooporder.";
         }
     } else {
         $message = "Vul alstublieft alle vereiste velden in.";
@@ -78,48 +57,42 @@ if (isset($_POST["insert"]) && $_POST["insert"] == "Toevoegen") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toevoegen Klant en Artikel</title>
+    <title>Toevoegen Verkooporder</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
 
-    <h1>CRUD Verkooporder</h1>
+    <h1>Verkooporder</h1>
     <h2>Toevoegen</h2>
     <?php if ($message): ?>
         <p><?php echo $message; ?></p>
     <?php endif; ?>
     <form method="post">
-        <label for="nv">Klantnaam:</label>
-        <input type="text" id="nv" name="klantnaam" placeholder="Klantnaam" required/>
+        <label for="klantId">Klant:</label>
+        <select id="klantId" name="klantId" required>
+            <option value="">Selecteer Klant</option>
+            <?php foreach ($klanten as $klant): ?>
+                <option value="<?php echo $klant['klantId']; ?>"><?php echo $klant['klantNaam']; ?></option>
+            <?php endforeach; ?>
+        </select>
         <br>
-        <label for="an">Klantemail:</label>
-        <input type="email" id="an" name="klantemail" placeholder="Klantemail" required/>
-        <br>
-        <label for="an">KlantWoonplaats:</label>
-        <input type="text" id="an" name="klantwoonplaats" placeholder="Klantwoonplaats" required/>
-        <br>
-        <label for="an">KlantPostcode:</label>
-        <input type="text" id="an" name="klantpostcode" placeholder="Klantpostcode" required/>
-        <br>
-        <label for="an">KlantAdres:</label>
-        <input type="text" id="an" name="klantadres" placeholder="Klantadres" required/>
-        <br>
-        <h3>Artikelgegevens</h3>
-        <label for="nv">Artikelnaam:</label>
-        <input type="text" id="nv" name="artikelnaam" placeholder="Artikelnaam" required/>
-        <br>
-        <label for="an">Artikelprijs:</label>
-        <input type="number" id="an" name="artikelprijs" placeholder="Artikelprijs" step="0.01" required/>
+        <label for="artId">Artikel:</label>
+        <select id="artId" name="artId" required>
+            <option value="">Selecteer Artikel</option>
+            <?php foreach ($artikelen as $artikel): ?>
+                <option value="<?php echo $artikel['artId']; ?>"><?php echo $artikel['artOmschrijving']; ?></option>
+            <?php endforeach; ?>
+        </select>
         <br>
         <h3>Verkoopordergegevens</h3>
-        <label for="vd">Verkooporder Datum:</label>
-        <input type="date" id="vd" name="verkOrdDatum" required/>
+        <label for="verkOrdDatum">Verkooporder Datum:</label>
+        <input type="date" id="verkOrdDatum" name="verkOrdDatum" required/>
         <br>
-        <label for="vb">Verkooporder Bestel Aantal:</label>
-        <input type="number" id="vb" name="verkOrdBestAantal" required/>
+        <label for="verkOrdBestAantal">Verkooporder Bestel Aantal:</label>
+        <input type="number" id="verkOrdBestAantal" name="verkOrdBestAantal" required/>
         <br>
-        <label for="vs">Verkooporder Status:</label>
-        <input type="number" id="vs" name="verkOrdStatus" required/>
+        <label for="verkOrdStatus">Verkooporder Status:</label>
+        <input type="number" id="verkOrdStatus" name="verkOrdStatus" required/>
         <br><br>
         <input type='submit' name='insert' value='Toevoegen'>
     </form></br>
